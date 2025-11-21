24bead670b93865a8e5863a600bbcf6080706077
srikanth1104 <srikanth555chinnu@gmail.com>
2025-11-21 17:28:35 +0530

    Added backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://task-manager-rbac-backend.onrender.com/api',
 });
 
-// Add token to every request if available
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
